ee259049541174f5a922c6e50d694a4c9281ab9e
faisalkamilansari <faisalkamilansari@gmail.com>
2024-08-31 15:12:04 +0530

    Rectified bash script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,5 +7,5 @@ kubectl apply -f deployments/elasticsearch/elasticsearch-deployment.yaml
 kubectl apply -f deployments/kibana/kibana-deployment.yaml
 
 # Apply Fluentd Configuration
-kubectl apply -f deployments/fluentd/fluentd-configmap.yaml
-kubectl apply -f deployments/fluentd/fluentd-daemonset.yaml
+kubectl apply -f deployments/fluentd/configmap.yaml
+kubectl apply -f deployments/fluentd/daemonset.yaml
