b0c6ace0e23d0e18fecbc09d2bb8c3e9debb4ed5
faisalkamilansari <faisalkamilansari@gmail.com>
2024-09-01 06:32:33 +0530

    Modified Script for kibana service

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,3 +9,4 @@ kubectl apply -f deployments/kibana/kibana-deployment.yaml
 # Apply Fluentd Configuration
 kubectl apply -f deployments/fluentd/configmap.yaml
 kubectl apply -f deployments/fluentd/daemonset.yaml
+kubectl apply -f deployments/kibana/service.yaml
