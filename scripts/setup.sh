13f3879e6fc4ec8aeb71520b68ff0e2434eb56c3
faisalkamilansari <faisalkamilansari@gmail.com>
2024-09-01 07:11:24 +0530

    Modified script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,6 +8,12 @@ kubectl apply -f deployments/kibana/kibana-deployment.yaml
 
 # Apply Fluentd Configuration
 kubectl apply -f deployments/fluentd/configmap.yaml
+
+# Apply Fluentd Daemonset
 kubectl apply -f deployments/fluentd/daemonset.yaml
+
+# Apply Elasticsearch Service
 kubectl apply -f deployments/elasticsearch/elasticsearch-service.yaml
+
+# Apply Kibana Service
 kubectl apply -f deployments/kibana/service.yaml
